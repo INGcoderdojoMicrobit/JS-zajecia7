a68b7d9b1ed3d4b1d81f5d621056a00c93facbae
Maks1116 <Maks_Lib@wp.pl>
2021-03-20 14:58:01 +0100

    Przeniosłem funkcję z palindromami do palindromy

--- zajecia7.js
+++ zajecia7.js
@@ -283,47 +283,10 @@ function SelectionSortWyraz() {
   }
 }
 
-function WyrazoweZabawy() {
-  var wyrazy = require("./wyrazy-1.json");
-  // wyświetlamy pierwsze 10wyrazów
-  //console.log(`wyrazy (pierwsze 10)`);
-  var tab = [];
-  for (
-    let iIter1 = 0;
-    iIter1 < wyrazy.length - 1;
-    iIter1++
-  ) /*{
-      console.log(`${wyrazy[iIter1].wyraz} palindrom: ${Palindromek(wyrazy[iIter1].wyraz) ? "tak" : "nie"}`);
-    }*/
-  {
-    if (Palindromek(wyrazy[iIter1].wyraz)) {
-      console.log(wyrazy[iIter1].wyraz);
-      tab.push(wyrazy[iIter1].wyraz);
-    }
-    //if (iIter1%10000==0) console.log(iIter1)
-  }
 
-  console.log(`wyrazow jest ${wyrazy.length}`);
-  console.log(tab);
-}
-
-function Palindromek(wejWyraz) {
-  //sprawdzanie czy jest palindromem
-  //console.log(`wyraz: ${wejWyraz}`);
-  var bJestPalindromem = true;
-  for (let iIter3 = 0; iIter3 < Math.floor(wejWyraz.length / 2); iIter3++) {
-    //console.log(`literka: ${iIter3}, ${wejWyraz[iIter3]}, od konca: ${wejWyraz.length-iIter3-1}, ${wejWyraz[wejWyraz.length-iIter3-1]}`);
-    if (wejWyraz[iIter3] != wejWyraz[wejWyraz.length - iIter3 - 1]) {
-      bJestPalindromem = false;
-      //console.log(`sorki - nie jestem palindromem: ${wejWyraz}`);
-      break; // nie jestem przerywam
-    }
-  }
-  return bJestPalindromem;
-}
 
 //ZliczajStatystyki();
-WyrazoweZabawy();
+
 //BubbleSort();
 //SelectionSort();
 //SelectionSortWyraz();
