9a896e899200c695a2ef391fb258e9ec09207739
INGcoderdojoMicrobit <>
2021-04-11 21:53:44 +0200

    kolejne API

--- httpreq.js
+++ httpreq.js
@@ -23,12 +23,12 @@ function OpenWeatherMap(sCity){
             //console.log(data);
             let oPogoda = JSON.parse(data);
             //console.log(oPogoda);
-            console.log(sCity);
-            console.log(`temperatura: ${oPogoda.main.temp}°C, temperatura odczuwalna: ${oPogoda.main.feels_like}°C, temp min: ${oPogoda.main.temp_min}°C, temp max: ${oPogoda.main.temp_max}°C`);
-            console.log(`ciśnienie: ${oPogoda.main.pressure}hPa, wilgotność: ${oPogoda.main.humidity}`);
-            console.log(`widoczność: ${oPogoda.visibility}m`);
+            console.log("openweather-> "+sCity);
+            console.log(`openweather-> temperatura: ${oPogoda.main.temp}°C, temperatura odczuwalna: ${oPogoda.main.feels_like}°C, temp min: ${oPogoda.main.temp_min}°C, temp max: ${oPogoda.main.temp_max}°C`);
+            console.log(`openweather-> ciśnienie: ${oPogoda.main.pressure}hPa, wilgotność: ${oPogoda.main.humidity}`);
+            console.log(`openweather-> widoczność: ${oPogoda.visibility}m`);
             
-            console.log(`http://openweathermap.org/img/w/${oPogoda.weather[0].icon}.png`)
+            console.log(`openweather-> http://openweathermap.org/img/w/${oPogoda.weather[0].icon}.png`)
     });
 
     }).on("error", (err) => {
@@ -53,7 +53,7 @@ function AccuWeatherLocations(country){
     resp.on('end', () => {
             console.log(data);
             let oWojewodztwa = JSON.parse(data);
-            console.log(oWojewodztwa);
+            console.log(`AccuWeather-> ${oWojewodztwa}`);
     });
 
     }).on("error", (err) => {
@@ -74,9 +74,9 @@ function AccuWeatherCitySearch(city,wojew){
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
-            console.log(data);
+            //console.log(data);
             let oMiasto = JSON.parse(data);
-            console.log(oMiasto);
+            console.log(`AccuWeather-> ${oMiasto}`);
     });
 
     }).on("error", (err) => {
@@ -100,7 +100,8 @@ function AccuWeather1hForecast(cityID){
     resp.on('end', () => {
             console.log(data);
             let o1hForecast = JSON.parse(data);
-            console.log(o1hForecast);
+            console.log(`AccuWeather-> temp:${o1hForecast[0].Temperature.Value}°${o1hForecast[0].Temperature.Unit}, link: ${o1hForecast[0].MobileLink}`);
+            console.log(`AccuWeather-> icon: https://www.accuweather.com/images/weathericons/${o1hForecast[0].WeatherIcon}.svg`);
             //console.log(`widoczność: ${oPogoda.visibility}m`);
             sWynik = o1hForecast;
     });
@@ -144,12 +145,12 @@ function NasaAPI(){
     
     request(`https://api.nasa.gov/planetary/earth/assets?lon=18.947329&lat=50.193520&date=2021-04-10&dim=0.20&api_key=${NASAAPPID}`, { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
-    console.log(body.url);
+    console.log("NASA-> "+ body.url);
     });
 
     request(`https://api.nasa.gov/planetary/apod?api_key=${NASAAPPID}`, { json: true }, (err, res, body) => {
             if (err) { return console.log(err); }
-            console.log(body.url);
+            console.log("NASA-> "+ body.url);
             GoogleTranslate(body.explanation, "auto", "pl")
     });
         
@@ -172,12 +173,88 @@ function ThingSpeakAPI(){
        tab.push(Math.floor(parseInt(body.feeds[iIter1].field3)));
     }
     
-    console.log(`rozkład PM10 Katowice Zarzecze - względem 70`);
+    console.log(`Thingspeak -> rozkład PM10 Katowice Zarzecze - względem 70`);
     api2.RysujHistogram(tab, false, 0, 70)
     //console.log(body.channel);
     });
 
 }
+
+
+
+function GeoIPAPI(){
+    
+    request(`https://freegeoip.app/json/`, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log(`freegeoIP -> IP:${body.ip} kraj:${body.country_name}, region:${body.region_name}`);
+    });
+}
+
+
+function CountriesAPI(){
+//https://restcountries.eu/#api-endpoints-all    
+    request(`https://restcountries.eu/rest/v2/all`, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        body.forEach(element => {
+            if (Math.random()*100<5) console.log(`restcountries-> kraj:${element.name} stolica:${element.capital}, region:${element.region}`);    
+        });
+    });
+}
+
+function CountryAPI(sName){
+    
+    request(`https://restcountries.eu/rest/v2/name/${sName}`, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log(`restcountries-> kraj:${body[0].name} stolica:${body[0].capital}, populacja:${body[0].population}`);    
+    });
+}
+
+
+function BooksISBNAPI(sISBN){
+//https://openlibrary.org/dev/docs/api/books    
+    let sAuthor="";
+    request(`https://openlibrary.org/isbn/${sISBN}.json`, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        sAuthor = body.authors[0].key;
+        console.log(`openlibrabry-> Tytuł:${body.title}, ID autora: ${sAuthor}`);    
+    });
+}
+
+
+function IPFyAPI(){
+    //https://api.ipify.org/?format=json
+        request(`https://api.ipify.org/?format=json`, { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+            console.log(`IPfy-> IP:${body.ip}`);    
+        });
+    }
+
+function IPinfoAPI(sIP){
+    //https://ipinfo.io/161.185.160.93/geo    
+        request(`https://ipinfo.io/${sIP}/geo`, { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+            console.log(`IPinfo-> miasto:${body.city}, lokalizacja: ${body.loc}, państwo: ${body.country} `);    
+        });
+}
+
+function ZippoAPI(sKodPoczt, sKraj){
+    //http://api.zippopotam.us/pl/40-748
+    //http://www.zippopotam.us/
+        request(`http://api.zippopotam.us/${sKraj}/${sKodPoczt}`, { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+            console.log(`zippo-> miasto:${body.places[0]["place name"]}, region:${body.places[0].state}`);    
+        });
+}
+
+function BoredAPI(iIleOsob){
+    //https://www.boredapi.com/
+    //https://www.boredapi.com/api/activity?participants=1
+            request(`https://www.boredapi.com/api/activity?participants=${iIleOsob}`, { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+            console.log(`Bored-> co robic:${body.activity}, link:${body.link}`);    
+        });
+}
+
 
 
 
@@ -188,6 +265,8 @@ function sleep(ms) {
 
 //AccuWeatherLocations("pl") //Polska
 //AccuWeatherCitySearch("Katowice",24) //24 - śląskie
+
+//tylko 50 wywołań dziennie
 //AccuWeather1hForecast(275781) //275781 - Katowice
 
 OpenWeatherMap("Katowice")
@@ -198,7 +277,11 @@ GoogleTranslate("get your ass here and stop fighiting me", "auto", "pl")
 
 NasaAPI();
 ThingSpeakAPI();
-//console.log(`wynik=${sWynik}`)
-
-
-      
+GeoIPAPI();
+IPFyAPI();
+CountriesAPI();
+CountryAPI("poland")
+BooksISBNAPI("9788366573536")
+IPinfoAPI("91.201.120.214");
+ZippoAPI("40-748","pl")
+BoredAPI(1)
